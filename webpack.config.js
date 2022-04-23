3d019cfb46a6445d427dd0ff22a61a39fb0d2ad5
Felipe Leão <felipe@felipeleao.com>
2022-04-23 10:03:52 -0300

    Create project

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path')
+
+module.exports = ({ mode = 'development' }) => ({
+  mode,
+  entry: './main.js',
+  output: {
+    path: __dirname + '/public',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
+  },
+})
